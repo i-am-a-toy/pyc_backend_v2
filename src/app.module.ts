b68a7a19d6c54b9ad85337b449ef1e068169db90
leewoooo <leewoooo.dev@gmail.com>
2023-01-28 18:12:02 +0900

    ⚡️Improve: add application module array

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/code.module';
+import { AuthModule } from './modules/auth/auth.module';
+
+const applicationModule = [AuthModule];
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, ...applicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
