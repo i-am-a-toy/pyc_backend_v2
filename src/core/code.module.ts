92f47a697332889056b0bee9697f19d218b32ee7
leewoooo <leewoooo.dev@gmail.com>
2023-01-28 18:12:35 +0900

    ⚡️Improve: core module array & providers array

--- src/core/code.module.ts
+++ src/core/code.module.ts
@@ -1,12 +1,18 @@
 import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TransactionManager } from './database/typeorm/transaction-manager';
 import { getTypeOrmModule } from './database/typeorm/typeorm.module';
 import { TransactionMiddleware } from './middleware/transaction.middleware';
+import { TokenModule } from './token/token.module';
 import { ShutDownManager } from './util/shutdown.manager';
+
+const modules = [TokenModule];
+const providers = [TransactionManager];
 
 @Global()
 @Module({
-  imports: [getTypeOrmModule()],
-  providers: [ShutDownManager],
+  imports: [getTypeOrmModule(), ...modules],
+  providers: [ShutDownManager, ...providers],
+  exports: [...modules, ...providers],
 })
 export class CoreModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
