636294cd43abf8a99a8f0fcd7d9a43f75e0dd0e3
leewoooo <leewoooo.dev@gmail.com>
2023-01-28 18:06:39 +0900

    🔨Modify: change from, to

--- src/core/database/typeorm/transformer/big-int.transformer.ts
+++ src/core/database/typeorm/transformer/big-int.transformer.ts
@@ -1,10 +1,11 @@
 import { ValueTransformer } from 'typeorm';
 
 export class BigintTransformer implements ValueTransformer {
-  to(value: string): number {
-    return Number(value);
-  }
-  from(value: number): string {
+  to(value: number): string {
     return value.toString();
+  }
+
+  from(value: string): number {
+    return Number(value);
   }
 }
