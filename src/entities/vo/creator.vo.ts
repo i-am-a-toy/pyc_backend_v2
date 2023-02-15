1b3a871afd38c1beae8ccc507dffd075b0346747
leewoooo <leewoooo@visionerp.org>
2023-02-15 16:12:21 +0900

    ✨Add: creator, lastModifier VO & test Code

---
+++ src/entities/vo/creator.vo.ts
@@ -0,0 +1,20 @@
+import { Role } from 'src/common/types/role/role.type';
+import { BigintTransformer, RoleTransformer } from 'src/core/database/typeorm/transformer';
+import { Column } from 'typeorm';
+
+export class CreatorVO {
+  @Column({ name: 'creator_id', type: 'bigint', nullable: false, transformer: new BigintTransformer() })
+  id: number;
+
+  @Column({ name: 'creator_name', type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ name: 'creator_role', type: 'varchar', nullable: false, transformer: new RoleTransformer() })
+  role: Role;
+
+  constructor(id: number, name: string, role: Role) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
+  }
+}
