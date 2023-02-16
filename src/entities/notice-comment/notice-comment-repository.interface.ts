f8c72a65fe9f55e75426ad9b8f2575b9ab685920
leewoooo <leewoooo.dev@gmail.com>
2023-02-16 23:22:03 +0900

    ⚡️Improve: append findAllByNoticeId, deleteById

--- src/entities/notice-comment/notice-comment-repository.interface.ts
+++ src/entities/notice-comment/notice-comment-repository.interface.ts
@@ -3,4 +3,7 @@ import { NoticeComment } from './notice-comment.entity';
 
 export const NoticeCommentRepositoryKey = 'NoticeCommentRepositoryKey';
 
-export interface INoticeCommentRepository extends GenericRepository<NoticeComment> {}
+export interface INoticeCommentRepository extends GenericRepository<NoticeComment> {
+  findAllByNoticeId(noticeId: number, offset: number, limit: number): Promise<[NoticeComment[], number]>;
+  deleteById(id: number): Promise<void>;
+}
--- src/entities/notice-comment/notice-comment.repository.ts
+++ src/entities/notice-comment/notice-comment.repository.ts
@@ -9,4 +9,17 @@ export class NoticeCommentRepository extends GenericTypeOrmRepository<NoticeComm
   getName(): EntityTarget<NoticeComment> {
     return NoticeComment.name;
   }
+
+  findAllByNoticeId(noticeId: number, offset: number, limit: number): Promise<[NoticeComment[], number]> {
+    return this.getRepository().findAndCount({
+      where: { noticeId },
+      skip: offset,
+      take: limit,
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  async deleteById(id: number): Promise<void> {
+    await this.getRepository().delete({ id });
+  }
 }
