df310b4fc80aa2a068b24c5bf5cd1c184c648ad2
leewoooo <leewoooo@visionerp.org>
2023-02-17 22:17:27 +0900

    🏷️Add: fromToVO

---
+++ src/entities/vo/from-to.vo.ts
@@ -0,0 +1,18 @@
+import { Column } from 'typeorm';
+
+export class FromToVO {
+  @Column({ type: 'timestamptz', nullable: false })
+  start!: Date;
+
+  @Column({ type: 'timestamptz', nullable: false })
+  end!: Date;
+
+  @Column({ type: 'boolean', nullable: false, default: true })
+  isAllDay!: boolean;
+
+  constructor(start: Date, end: Date, isAllDay: boolean) {
+    this.start = start;
+    this.end = end;
+    this.isAllDay = isAllDay;
+  }
+}
