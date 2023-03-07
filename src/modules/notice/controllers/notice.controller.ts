efd38571f2f284aa082d37375ec0674c7615bdd0
leewoooo <leewoooo@visionerp.org>
2023-03-07 18:18:27 +0900

    🔨Modify: append role base authentication

--- src/modules/notice/controllers/notice.controller.ts
+++ src/modules/notice/controllers/notice.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Inject, Param, ParseIntPipe, Post, Put, Query, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common/decorators';
 import { PycUser } from 'src/common/dto/context/pyc-user.dto';
 import { CreateNoticeRequest } from 'src/common/requests/notice/create-notice.request';
 import { FindNoticeListRequest } from 'src/common/requests/notice/find-notice-list.request';
@@ -6,6 +7,9 @@ import { UpdateNoticeRequest } from 'src/common/requests/notice/update-notice.re
 import { NoticeListResponse } from 'src/common/responses/notice/notice-list.response';
 import { NoticeResponse } from 'src/common/responses/notice/notice.response';
 import { PycContext } from 'src/core/decorator/pyc-context.decorator';
+import { Role } from '../../../common/types/role/role.type';
+import { AllowRole } from '../../../core/decorator/roles.decorator';
+import { RoleGuard } from '../../../core/guard/role.guard';
 import { INoticeService, NoticeServiceKey } from '../interfaces/notice-service.interface';
 
 @Controller('/notices')
@@ -24,16 +28,22 @@ export class NoticeController {
     return new NoticeResponse(notice);
   }
 
+  @UseGuards(RoleGuard)
+  @AllowRole(Role.GROUP_LEADER)
   @Post()
   async write(@PycContext() pycUser: PycUser, @Body() body: CreateNoticeRequest): Promise<void> {
     await this.service.write(pycUser, body.title, body.content);
   }
 
+  @UseGuards(RoleGuard)
+  @AllowRole(Role.GROUP_LEADER)
   @Put('/:id')
   async modify(@PycContext() pycUser: PycUser, @Param('id', ParseIntPipe) id: number, @Body() body: UpdateNoticeRequest): Promise<void> {
     await this.service.modify(pycUser, id, body.title, body.content);
   }
 
+  @UseGuards(RoleGuard)
+  @AllowRole(Role.GROUP_LEADER)
   @Delete('/:id')
   async deleteById(@Param('id', ParseIntPipe) id: number): Promise<void> {
     await this.service.deleteById(id);
